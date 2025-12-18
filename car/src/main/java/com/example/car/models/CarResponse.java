27993add8b08fba0638f243ed83bc2f9c376aff5
redxyoussef01 <yourais00@gmail.com>
2025-12-18 09:34:46 +0100

    getters

--- car/src/main/java/com/example/car/models/CarResponse.java
+++ car/src/main/java/com/example/car/models/CarResponse.java
@@ -1,19 +1,104 @@
 package com.example.car.models;
 
 import com.example.car.entities.Client;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Builder
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class CarResponse {
     private Long id;
     private String brand;
     private String model;
     private String matricue;
     private Client client;
+
+    public CarResponse() {
+    }
+
+    public CarResponse(Long id, String brand, String model, String matricue, Client client) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.matricue = matricue;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMatricue() {
+        return matricue;
+    }
+
+    public void setMatricue(String matricue) {
+        this.matricue = matricue;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    // Builder pattern implementation
+    public static CarResponseBuilder builder() {
+        return new CarResponseBuilder();
+    }
+
+    public static class CarResponseBuilder {
+        private Long id;
+        private String brand;
+        private String model;
+        private String matricue;
+        private Client client;
+
+        public CarResponseBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public CarResponseBuilder brand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public CarResponseBuilder model(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public CarResponseBuilder matricue(String matricue) {
+            this.matricue = matricue;
+            return this;
+        }
+
+        public CarResponseBuilder client(Client client) {
+            this.client = client;
+            return this;
+        }
+
+        public CarResponse build() {
+            return new CarResponse(id, brand, model, matricue, client);
+        }
+    }
 }
